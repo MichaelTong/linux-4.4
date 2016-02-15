21e7fce90166aa8abbcaa3ea0105277e19028015
MichaelTong <tonghao1992@gmail.com>
2016-02-15 15:29:44 -0600

    modify for 4.4: bio chain

--- block/bio.c
+++ block/bio.c
@@ -1758,7 +1758,8 @@ void bio_endio(struct bio *bio)
 			if(bio_flagged(parent, 9))
 			{
 				bio->e1 = ktime_get();
-				printk("MikeT: %s %s %d, %llu, %llu, %p\n", __FILE__, __func__, __LINE__, bio->bi_iter.bi_sector, ktime_to_ns(ktime_sub(bio->e1, bio->b1)), bio);
+				printk("MikeT: %s %s %d, %lu, %llu, %p\n", __FILE__, __func__, __LINE__, bio->bi_iter.bi_sector, ktime_to_ns(ktime_sub(bio->e1, bio->b1)), bio);
+				printk("MikeT: %s %s %d, %lu, %lu\n", __FILE__, __func__, __LINE__, parent->ori_sector, parent->bi_iter.bi_sector);
 				//(*(parent->bio_time))[(bio->bi_iter.bi_sector - parent->ori_sector)/bio_sectors(bio)] 
 				//			= ktime_to_ns(ktime_sub(bio->e1, bio->b1));
 			}
@@ -1769,7 +1770,7 @@ void bio_endio(struct bio *bio)
 			if(bio_flagged(bio, 9))
 			{
 				bio->e1 = ktime_get();
-				printk("MikeT: %s %s %d, %llu, %p\n", __FILE__, __func__, __LINE__, bio->bi_iter.bi_sector, ktime_to_ns(ktime_sub(bio->e1, bio->b1)), bio);
+				printk("MikeT: %s %s %d, %lu, %llu, %p\n", __FILE__, __func__, __LINE__, bio->bi_iter.bi_sector, ktime_to_ns(ktime_sub(bio->e1, bio->b1)), bio);
 				//(*(bio->bio_time))[(bio->bi_iter.bi_sector - bio->ori_sector)/bio_sectors(bio)] 
 				//			= ktime_to_ns(ktime_sub(bio->e1, bio->b1));
 			}
